0554d0c125ec714bc5d2e066d01cdb3c4aa29137
Belle P <bellepx3@gmail.com>
2019-07-11 01:56:00 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,15 +1,15 @@
 def addition(num1, num2)
-  sum = "#{num1}" + "#{num2}"
+  sum = num1 + num2
   puts sum
 end
 
 def subtraction(num1, num2)
-  difference = "#{num1}" - "#{num2}"
+  difference = num1 - num2
   puts difference
 end
 
 def division(num1, num2)
-  quotient = num1 / "#{num2}"
+  quotient = num1 / num2
   puts quotient
 end
 
